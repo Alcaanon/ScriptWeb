5a9d243710b3aa6e17fcac5c8c32262816adbc8c
Thalis Mateus <thalis.alcapone@gmail.com>
2022-10-23 19:23:09 -0300

    Aula 03

--- Senac/src/app/app.component.spec.ts
+++ Senac/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Senac'`, () => {
+  it(`should have as title 'tuxview'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Senac');
+    expect(app.title).toEqual('tuxview');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Senac app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tuxview app is running!');
   });
 });
--- Senac/src/app/app.component.ts
+++ Senac/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+//Diretivas do angular
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'Senac';
+  title = 'Data Binding';
+  descricao = "O data binding pega uma informação no arquivo .ts e leva para o arquivo. html";
+  imagem = "https://cdn.pixabay.com/photo/2022/10/05/20/43/hyacinth-macaw-7501470__340.jpg";
+  des = "Papagaio";
+
+  //Declarando um objeto quando coloco as ={}
+  pessoa = {
+    nome:"Thalis Mateus",
+    idade: 28,
+    altura: 1.70,
+    aniversario: "28/01/1994"
+  };
+
+  fotos = {
+    img: ""
+  }
+  catalogoFotos = [
+    {img: "https://i.imgur.com/27Fu23S.jpg"},
+    {img: "https://imgur.com/iXTbTNQ.jpg"},
+    {img: "https://imgur.com/ngVf76n.jpg"},
+    {img: "https://imgur.com/Rqf8EhW.jpg"},
+    {img: "https://imgur.com/OeKSUpk.jpg"},
+  ]
+  
+  meuCliente = [
+ 
+    { nome: "Joao", idade:15 },
+    { nome: "Mara", idade:35 },
+    { nome: "Maria", idade:35 },
+    { nome: "silva", idade:11 },
+  ];
 }
