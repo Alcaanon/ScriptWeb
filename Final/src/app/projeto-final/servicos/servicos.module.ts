958401a52b97b02f98085266dbaca1d497b43abb
Thalis Mateus <thalis.alcapone@gmail.com>
2022-11-21 19:43:32 -0300

    Correção

--- Final/src/app/projeto-final/servicos/servicos.module.ts
+++ Final/src/app/projeto-final/servicos/servicos.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServicoFormularioComponent } from './servico-formulario/servico-formulario.component';
+import { ServicoDetalhesComponent } from './servico-detalhes/servico-detalhes.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ServicoFormularioComponent,
+    ServicoDetalhesComponent
+  ],
   imports: [
     CommonModule
   ]
