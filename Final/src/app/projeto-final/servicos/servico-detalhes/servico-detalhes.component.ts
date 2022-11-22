63f9523cebc7fedb672460b71f66c9c1415ccf7d
Thalis Mateus <thalis.alcapone@gmail.com>
2022-11-21 21:00:11 -0300

    CRUD - SERVICO

--- Final/src/app/projeto-final/servicos/servico-detalhes/servico-detalhes.component.ts
+++ Final/src/app/projeto-final/servicos/servico-detalhes/servico-detalhes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Servico } from 'src/assets/Interfaces/Servico';
+import { DatabaseService } from '../../database.service';
 
 @Component({
   selector: 'app-servico-detalhes',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoDetalhesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private database: DatabaseService
+  ) { }
+
 
   ngOnInit(): void {
+    this.ValidaForm();
   }
 
+  formulario!: FormGroup;
+
+  servico: Servico = {
+    id: 0,
+    imagem: "",
+    titulo:"",
+    descricao: ""
+  }
+  submitted = false;
+
+  ValidaForm(){
+    this.formulario = this.formBuilder.group({
+      imagem: ['', [Validators.required]],
+      titulo: ['', [Validators.required]],
+      descricao: ['', [Validators.required]],
+    });
+  }
+
+  cadastro(): void{
+    const data = {
+      imagem: this.servico.imagem,
+      titulo: this.servico.titulo,
+      descricao: this.servico.descricao
+    };
+    this.database.postServico(data)
+    .subscribe({
+      next: (res) => {
+        console.log(res);
+        this.submitted = true;
+      },
+      error: (e) => console.error(e)
+    });
+
 }
+
+}
+
